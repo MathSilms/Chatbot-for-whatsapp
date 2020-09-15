425e71bf57b212d207facad4e6bc760526f72309
Matheus Oliveira <62664418+MathSilms@users.noreply.github.com>
2020-09-15 15:04:37 -0300

    ...

--- src/index.js
+++ src/index.js
@@ -1,25 +1,14 @@
-// Supports ES6
-// import { create, Whatsapp } from 'sulla';
-//import { create, Whatsapp } from 'venom-bot';
 const banco = require('./database/banco')
 const stages = require('./stages')
 const venonbt = require('venom-bot');
 
 venonbt.create().then((client) => start(client));
-
 function start(client) {
   client.onMessage((message) => {
-    console.log(message)
-    if (message.body === 'Hi') {
-      client.sendText(message.from, 'Olá, seja bem vindo');
-    }
+    client.sendText(message.from, stages[getStage(message.from)].obj.execute());
   });
 }
 
 function getStage(user){
   return banco.db[user].stage;
 }
-
-console.log(stages.step[getStage("user2")].obj.execute());
-console.log(stages.step[getStage("user1")].obj.execute());
- 
