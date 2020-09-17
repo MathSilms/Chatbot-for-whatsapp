2d850c6926240998ea758d85532f85e88e2e9a59
Matheus Oliveira <62664418+MathSilms@users.noreply.github.com>
2020-09-17 16:20:28 -0300

    Send menu

--- src/stages/0.js
+++ src/stages/0.js
@@ -1,7 +1,16 @@
 const menu  = require('../menu')
 
 function execute(){
-    return ['Olá, seja bem vindo. Sou seu assistente virtual!']
+
+    let List = " Cardápio \n\n";
+
+
+    Object.keys(menu.menu).forEach(value =>{
+        let element = menu.menu[value]
+        List += `${element} - ${element.description}    R$ ${element.prince} \n`;
+    });
+
+    return ['Olá, seja bem vindo. Sou seu assistente virtual!', menu[{Object}] ]
 }
 
 exports.execute = execute;
