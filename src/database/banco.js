a9e554fc711ff885abe086be3aff4eeda65488d9
Matheus Oliveira <62664418+MathSilms@users.noreply.github.com>
2020-09-22 04:20:34 -0300

    add items to the database

--- src/database/banco.js
+++ src/database/banco.js
@@ -1,9 +1,11 @@
 const banco = {
     "5522998868691@c.us":{
       stage:0,
+      itens:[],
     },
     user2:{
       stage:1,
+      itens:[],
     }
 }
 
--- src/stages/1.js
+++ src/stages/1.js
@@ -1,5 +1,16 @@
-function execute(){
-    return 'Olá, seja bem vindo. Sou seu assistente virtual!'
+const options = require('../options');
+const banco = require('../database/banco')
+
+function execute(user, message ){
+
+    if(!options.menu[message]){
+        return ['Código inválido, digite o número correto. ','``` Digite # para finalizar ou * para cancelar```']
+    }
+
+    banco.db[user].itens.push(options.menu[message]);
+
+
+    return [`item ${options.menu[message].description} adicionado com sucesso`, '``` Digite # para finalizar ou * para cancelar```' ]
 }
 
 exports.execute = execute;
