ecfe59e95210e29fb1c41a7dd6e0daf794d58c02
Matheus Oliveira <62664418+MathSilms@users.noreply.github.com>
2020-09-22 14:25:55 -0300

    cancelar e seguir para o próximo

--- src/stages/1.js
+++ src/stages/1.js
@@ -3,14 +3,30 @@ const banco = require('../database/banco')
 
 function execute(user, message ){
 
+    if(message === '*'){
+        banco.db[user].stage = 0;
+        return ['Pedido Cancelado com sucesso'];
+    }
+
+    if(message === '#'){
+        banco.db[user].stage = 2
+        return ['Estamos fechando seu pedido, ok?']
+    }
+
     if(!options.menu[message]){
-        return ['Código inválido, digite o número correto. ','``` Digite # para finalizar ou * para cancelar```']
+        return [
+            'Código inválido, digite o número correto. ',
+            '``` Digite # para finalizar ou * para cancelar```'
+        ]
     }
 
     banco.db[user].itens.push(options.menu[message]);
 
 
-    return [`item ${options.menu[message].description} adicionado com sucesso`, '``` Digite # para finalizar ou * para cancelar```' ]
+    return [
+        `item ${options.menu[message].description} adicionado com sucesso`, 
+        '``` Digite # para finalizar ou * para cancelar```' 
+    ]
 }
 
 exports.execute = execute;
--- src/stages/2.js
+++ src/stages/2.js
@@ -1,5 +1,29 @@
-function execute(){
-    return 'Olá, seja bem vindo. Sou seu assistente virtual!'
+const banco = require('../database/banco')
+
+
+function execute(user, message){
+    
+    if(message === '*'){
+        banco.db[user].stage = 0;
+        return ['Pedido Cancelado com sucesso'];
+    }
+
+    if(message === '#'){
+        banco.db[user].stage = 3
+        return ['Estamos fechando seu pedido, ok?']
+    }
+
+    let resume = ` Resumo do pedido \n`;
+    let total = 0;
+    banco.db[user].itens.forEach( ( value ) =>{
+        total += value.price;
+        resume += `${velue.description} - - - - - - - -  ${value.price} \n`
+    });
+    
+    resume += ' - - - - - - - - - - - - - - - \n';
+    resume += `Total R$ ${total}`;
+    
+    return [resume,' - - - - - - - - - ' ,`Seu total é : ${total}`]
 }
 
 exports.execute = execute;
